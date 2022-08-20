72f21a002111399697d04aaa0f3e5b6aa499662c
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-08-20 22:30:31 +0300

    Task25

---
+++ Task25.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число А:");
+long a = Convert.ToInt64(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+long b = Convert.ToInt64(Console.ReadLine());
+long pow = a;
+for (int count = 1; count < b;) {
+a = a * pow;
+count += 1;
+}
+Console.WriteLine(a);
